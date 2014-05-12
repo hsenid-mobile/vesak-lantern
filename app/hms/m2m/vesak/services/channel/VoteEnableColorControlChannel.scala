75eec54bf9b9d404ca5df3af95cd041f4f86fcd3
isuruw <isuruw@hsenidmobile.com>
2014-05-13 01:01:03 +0530

    update the vote counter logics.

--- app/hms/m2m/vesak/services/channel/VoteEnableColorControlChannel.scala
+++ app/hms/m2m/vesak/services/channel/VoteEnableColorControlChannel.scala
@@ -52,7 +52,7 @@ trait VoteEnableColorControlChannel extends ColorControlChannel {
 
     scheduler.schedule(
       initialDelay = Duration(5, TimeUnit.SECONDS),
-      interval = Duration(10, TimeUnit.SECONDS),
+      interval = Duration(2, TimeUnit.SECONDS),
       runnable = task)
   }
 
@@ -70,7 +70,7 @@ trait VoteEnableColorControlChannel extends ColorControlChannel {
 
     scheduler.schedule(
       initialDelay = Duration(5, TimeUnit.SECONDS),
-      interval = Duration(2, TimeUnit.MINUTES),
+      interval = Duration(10, TimeUnit.MINUTES),
       runnable = task)
   }
 
