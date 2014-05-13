0d92683059ce5dae8a29c3db248839fa874f552f
isuruw <isuruw@hsenidmobile.com>
2014-05-13 14:30:29 +0530

    intensity calculation updated - cumilative weighted count considered.

--- app/hms/m2m/vesak/services/channel/ColorIntensityController.scala
+++ app/hms/m2m/vesak/services/channel/ColorIntensityController.scala
@@ -2,41 +2,67 @@ package hms.m2m.vesak.services.channel
 
 import hms.m2m.vesak.model.LanternColor
 import com.typesafe.scalalogging.slf4j.Logging
+import akka.actor.ActorSystem
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
 
 object ColorIntensityController extends Logging{
 
-  val channels = List[ColorControlChannel](InternalColorControlChannel, HttpColorControlChannel, SmsColorControlChannel)
+  val channels = List[VoteEnableColorControlChannel](HttpColorControlChannel, SmsColorControlChannel)
+
+  var nextIntensity = LanternColor(red = 0, green = 0, blue = 0)
+
+  counterUpdateScheduler
 
   def currentIntensity : LanternColor = {
+      nextIntensity
+  }
 
-    def compareWeightedSum(c1 : (LanternColor, Int), c2: (LanternColor, Int)) : (LanternColor, Int) = {
+  def counterUpdateScheduler {
+    val actorSystem = ActorSystem()
+    val scheduler = actorSystem.scheduler
+    val task = new Runnable {
+      def run() {
+        synchronized {
+          nextIntensity = calculateForNextCycle
+        }
+      }
+    }
+    implicit val executor = actorSystem.dispatcher
 
-      def max(i1 : Int, w1 : Int, i2 : Int, w2 : Int) : Int = {
-        if(i1 * w1 > i2 * w2) {
-          i1
+    scheduler.schedule(
+      initialDelay = Duration(5, TimeUnit.SECONDS),
+      interval = Duration(2, TimeUnit.SECONDS),
+      runnable = task)
+  }
+
+  def calculateForNextCycle : LanternColor = {
+
+    val colors: List[(Int, Int, Int)] = channels.map(c => (c.red.get() * c.channelWeight.get(),
+                                                            c.green.get() * c.channelWeight.get(),
+                                                            c.blue.get() * c.channelWeight.get()))
+
+    val finalWeightedVotes: (Int, Int, Int) = colors.reduce((c1, c2) => (c1._1 + c2._1, c1._2 + c2._2, c1._3 + c2._3))
+
+    val votes = List(finalWeightedVotes._1, finalWeightedVotes._2, finalWeightedVotes._3)
+
+    val totalVotes = votes.reduce((c1, c2) => c1 + c2)
+
+    if(totalVotes == 0) {
+      InternalColorControlChannel.currentIntensity
+    } else {
+      def getRank(i : Int) : Int = {
+        if(votes.max == i) {
+          255
         } else {
-          i2
+          (255 * i) / votes.max
         }
       }
 
-      (LanternColor(
-        red = max(c1._1.red,  c1._2, c2._1.red,  c2._2),
-        green = max(c1._1.green, c1._2, c2._1.green, c2._2),
-        blue = max(c1._1.blue, c1._2, c2._1.blue, c2._2)
-      ) , 1)
+      LanternColor(red = getRank(finalWeightedVotes._1), green = getRank(finalWeightedVotes._2), blue = getRank(finalWeightedVotes._3))
     }
 
-    channels.foreach(c1 =>
-            logger.debug("Channel " + c1.getClass.getName + " " + "Weight " + c1.channelWeight.get() + " " + "Intensity " + c1.currentIntensity)
-    )
-
-    val reduce: (LanternColor, Int) = channels.map(c1 => (c1.currentIntensity, c1.channelWeight.get())).reduce((color1, color2) => compareWeightedSum(color1, color2))
-
-    logger.debug("Final calculated intensity " + reduce._1)
-
-    reduce._1
   }
-
 
 
 }
---
+++ scripts/vote-via-sms.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type:application/json" -d '{"applicationId": "APP_000101", "sourceAddress": "94775038419", "message" : "vsk g", "requestId" : "reqiii"}' 'http://127.0.0.1:9000/sms'
